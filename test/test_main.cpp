0792100807b5671e9db483515a2172b23a7eea38
NikitaOrehov <Ornik0005@yandex.ru>
2025-02-08 16:05:57 +0300

    08.02.2025

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,16 +1,17 @@
 #include <gtest/gtest.h>
 #include "Vector.h"
-
-//int argc, char** argv
+#include <iostream>
+//
+////int argc, char** argv
+//
 
 int main(int argc, char** argv)
 {
-	/*::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();*/
-	//Vector a(3);
-	//Vector b(3);
-	//Vector c = a + b;
-	//std::cout << "test_main\n";
+	std::cout << "test\n";
+	Vector a(3);
+	Vector b(3);
+	Vector c = a + b;
+	std::cout << "test_main\n";
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
